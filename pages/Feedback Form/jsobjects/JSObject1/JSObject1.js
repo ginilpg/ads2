2822aad9775de126fc5ac3bc940bfb94bd4c1525
Ginil Pg <ginil.pg@gmail.com>
2023-05-22 13:45:54 +0000

    System generated commit, initial commit

---
+++ pages/Feedback Form/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,11 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: () => {
+		//write code here
+		return(Switch1.isSwitchedOn)
+	},
+	myFun2: async () => {
+		//use async-await or promises
+	}
+}
